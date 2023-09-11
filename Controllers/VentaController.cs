6202c5d57e0fba9db95b18037926b65cd2789273
alexander <alguefer16@gmail.com>
2023-09-11 11:45:15 -0500

    Se agrega al proyecto

---
+++ Controllers/VentaController.cs
@@ -0,0 +1,21 @@
+﻿
+using Gaming_Store.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gaming_Store.Controllers
+{
+    public class VentaController : Controller
+    {
+        private readonly ILogger<VentaController> _logger;
+
+        public VentaController(ILogger<VentaController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult NuevaVenta()
+        {
+            return View();
+        }
+    }
+}
